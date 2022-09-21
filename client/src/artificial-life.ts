2993bcd138222c0730289320b6949523d5a96d63
Konstantin Mazur <incerta.dei@gmail.com>
2022-09-21 21:50:50 +0200

    Abstract particles interaction/generation to function config

--- client/src/artificial-life.ts
+++ client/src/artificial-life.ts
@@ -1,3 +1,158 @@
-export function run() {
-  console.log(`%c HI`, 'color: purple; font-weight: 900; font-size: 14px')
+type ParticleGroup = {
+  id: string
+
+  /* Amount of particles generated */
+  amount: number
+
+  /* Color of each particle */
+  color: string
+
+  /* How this group affected by other particles */
+  affectedBy: Array<{
+    particleGroupId: string
+
+    /* Interaction magnitude: negative values is attraction and positive repultion */
+    gravity: number
+  }>
+}
+
+type Config = {
+  particleInteractionDistancePx: number
+  particleSizePx: number
+
+  /* Multiply velocity by provided value on each render cycle */
+  velocityMultiplier: number
+
+  canvasSize: {
+    width: number
+    height: number
+  }
+
+  particleGroups: ParticleGroup[]
+}
+
+type Particle = {
+  x: number
+  y: number
+
+  /* velocity X */
+  vx: number
+
+  /* velocity Y */
+  vy: number
+  color: string
+}
+
+export default function runArtificialLife(canvasContext: CanvasRenderingContext2D, cfg: Config) {
+  const draw = (x: number, y: number, color: string, sizePx: number) => {
+    canvasContext.fillStyle = color
+    canvasContext.fillRect(x, y, sizePx, sizePx)
+  }
+
+  const drawParticle = (particle: Particle) => {
+    draw(particle.x, particle.y, particle.color, cfg.particleSizePx)
+  }
+
+  const particleGroups: Map<string /* particleGroupId */, ParticleGroup & { particles: Particle[] }> = new Map()
+
+  cfg.particleGroups.forEach((particleGroup) => {
+    particleGroups.set(particleGroup.id, {
+      ...particleGroup,
+      particles: generateParticles({
+        canvasSize: cfg.canvasSize,
+        amount: particleGroup.amount,
+        color: particleGroup.color,
+      }),
+    })
+  })
+
+  const applyPhysics = (particles: Particle[], affectedBy: Particle[], gravity: number) => {
+    for (const a of particles) {
+      let forceX = 0
+      let forceY = 0
+
+      for (const b of affectedBy) {
+        if (a === b) continue
+
+        const distanceX = a.x - b.x
+        const distanceY = a.y - b.y
+
+        const distance = Math.sqrt(distanceX ** 2 + distanceY ** 2)
+
+        if (distance > 0 && distance < cfg.particleInteractionDistancePx) {
+          const force = gravity * (1 / distance)
+
+          forceX += force * distanceX
+          forceY += force * distanceY
+        }
+      }
+
+      a.vx = (a.vx + forceX) * cfg.velocityMultiplier
+      a.vy = (a.vy + forceY) * cfg.velocityMultiplier
+
+      a.x += a.vx
+      a.y += a.vy
+
+      if (a.x <= 0 || a.x >= cfg.canvasSize.width) a.vx *= -1
+      if (a.y <= 0 || a.y >= cfg.canvasSize.height) a.vy *= -1
+    }
+  }
+
+  const renderParticles = () => {
+    canvasContext.clearRect(0, 0, 500, 500)
+
+    particleGroups.forEach((group) => {
+      for (const affectedBy of group.affectedBy) {
+        const affectedByParticles = particleGroups.get(affectedBy.particleGroupId)
+
+        if (affectedByParticles === undefined) {
+          throw Error(`Non consistent config, can not found group with id: ${affectedBy.particleGroupId}`)
+        }
+
+        applyPhysics(group.particles, affectedByParticles.particles, affectedBy.gravity)
+      }
+    })
+
+    particleGroups.forEach((group) => group.particles.forEach(drawParticle))
+    requestAnimationFrame(renderParticles)
+  }
+
+  renderParticles()
+}
+
+function generateParticles(options: {
+  paddingPx?: number
+  canvasSize: {
+    width: number
+    height: number
+  }
+  amount: number
+  color: string
+}): Particle[] {
+  const particles: Particle[] = []
+  const paddingPx = options.paddingPx || 0
+
+  const makeParticle = (x: number, y: number, color: string): Particle => ({
+    x,
+    y,
+    color,
+    vx: 0,
+    vy: 0,
+  })
+
+  for (let i = 0; i < options.amount; i++) {
+    particles.push(
+      makeParticle(
+        random(paddingPx, options.canvasSize.width),
+        random(paddingPx, options.canvasSize.height),
+        options.color
+      )
+    )
+  }
+
+  return particles
+}
+
+function random(min: number, max: number) {
+  return Math.random() * (max - min) + min
 }
--- client/src/index.ts
+++ client/src/index.ts
@@ -1,3 +1,73 @@
-import { run } from './artificial-life'
+import runArtificialLife from './artificial-life'
 
-run()
+const rootEl = document.getElementById('root')
+
+if (rootEl === null) throw Error("Could not find 'root' element")
+
+const canvasEl = document.createElement('canvas')
+
+rootEl.append(canvasEl)
+
+canvasEl.setAttribute('width', '500')
+canvasEl.setAttribute('height', '500')
+
+const canvasContext = canvasEl.getContext('2d')
+
+if (canvasContext === null) throw Error("Could not find '2d' context")
+
+const YELLOW_ID = 'yellow'
+const RED_ID = 'red'
+const GREEN_ID = 'green'
+const MAGENTA_ID = 'magenta'
+
+runArtificialLife(canvasContext, {
+  particleInteractionDistancePx: 120,
+  particleSizePx: 1,
+  velocityMultiplier: 0.5,
+  canvasSize: {
+    width: 500,
+    height: 500,
+  },
+  particleGroups: [
+    {
+      id: YELLOW_ID,
+      color: 'yellow',
+      amount: 500,
+      affectedBy: [
+        { particleGroupId: RED_ID, gravity: 0.15 },
+        { particleGroupId: GREEN_ID, gravity: 0.15 },
+        { particleGroupId: YELLOW_ID, gravity: 0.15 },
+      ],
+    },
+    {
+      id: RED_ID,
+      color: 'red',
+      amount: 500,
+      affectedBy: [
+        { particleGroupId: RED_ID, gravity: 0.1 },
+        { particleGroupId: GREEN_ID, gravity: -0.1 },
+      ],
+    },
+    {
+      id: GREEN_ID,
+      color: 'green',
+      amount: 500,
+      affectedBy: [
+        { particleGroupId: YELLOW_ID, gravity: 0.2 },
+        { particleGroupId: GREEN_ID, gravity: -0.7 },
+        { particleGroupId: RED_ID, gravity: -0.2 },
+      ],
+    },
+    {
+      id: MAGENTA_ID,
+      color: 'magenta',
+      amount: 2000,
+      affectedBy: [
+        { particleGroupId: MAGENTA_ID, gravity: 0.4 },
+        { particleGroupId: GREEN_ID, gravity: -0.3 },
+        { particleGroupId: RED_ID, gravity: 0.2 },
+        { particleGroupId: YELLOW_ID, gravity: 1.1 },
+      ],
+    },
+  ],
+})
